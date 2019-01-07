212b799c9dc74a14fd7296538a135801db78f8c1
Denilson <DenilsonSeifer@gmail.com>
2019-01-07 16:41:54 -0500

    Segundo Teste

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Despesa(models.Model):
+    data_criacao= models.DateField()
+    tipo_despesa= models.CharField(max_lenght=50)
+    descricao= models.TextField()
+    forma_pagamento= models.CharField(max_lenght=50)
+    vencimento= models.DateField()
+g
